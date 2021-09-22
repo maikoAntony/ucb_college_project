867378acc81ff53ad1a5f22a0ea5fb79eb4ca7f6
maikoAntony <antony.maiko@outlook.com>
2021-09-22 02:41:29 -0300

    Iniciando Projeto

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main extends JFrame{
+    private JButton button1;
+    private JPanel rootPanel;
+
+    public Main(){
+        this.setContentPane(rootPanel);
+        this.setSize(800, 450);
+        this.setVisible(true);
+    }
+}
